734954ab72fb8b959faae6141731bedf69f098f0
yangfeiliang <yangfeiliang@cias.cn>
2020-07-13 17:44:01 +0800

    init

---
+++ js/content-script.js
@@ -0,0 +1,9 @@
+var name = '来去之间'
+var dom = document.querySelector('.gn_nav_list li:last-of-type .S_txt1')
+if (dom) {
+    dom.innerHTML = name
+}
+var dom2 = document.querySelector('.name.S_txt1')
+if (dom2) {
+    dom2.innerHTML = name
+}
