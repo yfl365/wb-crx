fd76a76db1ec873e9d96ccf190554f5699d632ca
yangfeiliang <yangfeiliang@cias.cn>
2020-07-13 18:21:05 +0800

    更新

--- js/content-script.js
+++ js/content-script.js
@@ -1,9 +1,22 @@
-var name = '来去之间'
-var dom = document.querySelector('.gn_nav_list li:last-of-type .S_txt1')
-if (dom) {
-    dom.innerHTML = name
-}
-var dom2 = document.querySelector('.name.S_txt1')
-if (dom2) {
-    dom2.innerHTML = name
+setName()
+window.addEventListener('visibilitychange', function () {
+    var num = 0
+    var timer = setInterval(() => {
+        num++
+        setName()
+        if (num >= 100) {
+            clearInterval(timer)
+        }
+    }, 16)
+})
+function setName() {
+    var name = '来去之间'
+    var dom = document.querySelector('.gn_nav_list li:last-of-type .S_txt1')
+    if (dom) {
+        dom.innerHTML = name
+    }
+    var dom2 = document.querySelector('.name.S_txt1')
+    if (dom2) {
+        dom2.innerHTML = name
+    }
 }
